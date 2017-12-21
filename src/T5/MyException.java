bb2980a4a8b0aaca7d5f01fab0afa04ee96b1061
qihang <qihang@ifeng.com>
2017-12-21 16:03:45 +0800

    Mytime 20讲，异常处理，分类

--- src/T5/MyException.java
+++ src/T5/MyException.java
@@ -20,19 +20,24 @@ public class MyException {
     public static void main(String[] args) {
         int a,b,c;
         a = 10;
-        b = 0;
+        b = 10;
         int aa[] = {1,2,3,4};
         try{
             c = a/b;
-            for (int i =0;i<aa.length;i++){
+            for (int i =0;i<=aa.length;i++){
                 System.out.println(aa[i]);
             }
+        }catch(ArithmeticException e1){
+            System.out.println("分母不能为0");
+        }catch(ArrayIndexOutOfBoundsException e2){
+            System.out.println("数组越界，超出了最小或者最大下标");
+
         }catch(Exception e){
             //打印堆栈的标准打印流，输出的错误信息比getMessage函数返回信息更详细
             e.printStackTrace();
             //返回异常的原因
             System.out.println(e.getMessage());
-            System.out.println("分母不能为0");
+            //System.out.println("分母不能为0");
             return;
         }finally {
             System.out.println("END....");
